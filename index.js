7e6f4266294ddec46858b59dbb57106af0d2fffe
maria liza <mariamartinez@pursuit.org>
2020-01-19 13:01:00 -0500

    finish up to exercise 2

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ SELECT title, year FROM movies;
 // Find all the information about each film
 
 SELECT * FROM movies;
+
+// Exercise 2 — Tasks
+// Find the movie with a row id of 6 ✓
+SELECT title FROM movies WHERE id = 6;
+// Find the movies released in the years between 2000 and 2010
+
+SELECT title FROM movies WHERE year BETWEEN 2000 AND 2010;
+
+// Find the movies not released in the years between 2000 and 2010
+
+SELECT title FROM movies WHERE year NOT BETWEEN 2000 AND 2010;
+
+// Find the first 5 Pixar movies and their release year
+
+SELECT title, year FROM movies
+WHERE year <= 2003;
